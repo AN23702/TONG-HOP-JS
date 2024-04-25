1231a208fad45dcaab2c4b224e6502d236e983ee
AN23702 <161464637+AN23702@users.noreply.github.com>
2024-04-25 22:10:11 +0700

    25/04/2024

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// console.log
+console.log("xin chao ngon ngu");
+
+// Biến
+let a = 23;
+const b = "Nguyễn Đức An";
+
+console.log(a);
+console.log(b);
+
+// Phạm vi của biến
+let d = 3;
+let numnbers = () => {
+    console.log(d);
+    let f = 5;
+    console.log(f);
+}
+
+numnbers();
+
+// Back-tick
+let name = "Nguyễn Đức An";
+let bornYear = 2002;
+let thisYear = 2024;
+
+console.log(`Tôi tên là ${name}, năm nay tôi ${thisYear - bornYear} tuổi, "khá già"`);
